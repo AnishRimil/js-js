32128399e0ae5dd5355fc94220941e439010760c
AnishRimil <149505465+AnishRimil@users.noreply.github.com>
2025-06-12 15:11:34 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+//alert(2+2) since we are using node js 
+//console.log(2+2);console.log("anish") not readable
+console.log(2+2)
+console.log("anish")
+
+let name ="Anish"
+let age = 20
+let isloggedIn = true
+ 
+
+//number = 2 to the power 53
+//bigint used in trading ,reddit
+//string = can be used in '',""
+//bolean = true or false 
+//null = stand alone value  it is a type its not undifined it is empty
+//undefined = value is not defined yet
+//symbol=  find uniqueness 
+
+
+//object = 
+
+console.log(typeof null);
