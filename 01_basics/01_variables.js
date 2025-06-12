9aca8dbf48f798d74e1fa4ad80437ed474808075
AnishRimil <149505465+AnishRimil@users.noreply.github.com>
2025-06-12 15:10:58 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 22442
+let accountEmail = "anish@gamil.com"
+var accountpassword = "34324"
+accountCity = "Ranchi"
+
+//accountId = 2
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountCity])
