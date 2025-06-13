eeebaaeb6857a9557dc279ce0a11d38aa2f84340
AnishRimil <149505465+AnishRimil@users.noreply.github.com>
2025-06-13 06:29:26 +0000

    conversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,18 @@
+ score="55"
+
+console.log(typeof score);
+
+ let valueInNumber = Number(score)//gurantee it will pass a number 
+
+console.log(typeof valueInNmber);
+console.log(valueInNumber)
+//"55"= 55
+//"55abc"=NAN
+  
+ // Conversion 
+let isloggedIn = 1
+let booleanIsloggedIn = Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
+// 1 = true
+// 0 = false
+// "anish" = true 
